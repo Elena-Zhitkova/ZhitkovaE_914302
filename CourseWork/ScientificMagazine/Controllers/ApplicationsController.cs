ecbcf3414d110e1e886d8152da3f8bc2a06197b6
Elena-Zhitkova <49303240+Elena-Zhitkova@users.noreply.github.com>
2023-02-27 14:50:40 +0300

    lab 1 - done

---
+++ CourseWork/ScientificMagazine/Controllers/ApplicationsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScientificMagazine.Data;
+using ScientificMagazine.Models;
+
+namespace ScientificMagazine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Applications
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Application>>> GetApplications()
+        {
+            return await _context.Applications.ToListAsync();
+        }
+
+        // GET: api/Applications/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Application>> GetApplication(int id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return application;
+        }
+
+        // PUT: api/Applications/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplication(int id, Application application)
+        {
+            if (id != application.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(application).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Applications
+        [HttpPost]
+        public async Task<ActionResult<Application>> PostApplication(Application application)
+        {
+            _context.Applications.Add(application);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApplication", new { id = application.Id }, application);
+        }
+
+        // DELETE: api/Applications/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplication(int id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            _context.Applications.Remove(application);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ApplicationExists(int id)
+        {
+            return _context.Applications.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CourseWork/ScientificMagazine/Controllers/ArchivesController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScientificMagazine.Data;
+using ScientificMagazine.Models;
+
+namespace ScientificMagazine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArchivesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArchivesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Archives
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Archive>>> GetArchives()
+        {
+            return await _context.Archives.ToListAsync();
+        }
+
+        // GET: api/Archives/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Archive>> GetArchive(int id)
+        {
+            var archive = await _context.Archives.FindAsync(id);
+
+            if (archive == null)
+            {
+                return NotFound();
+            }
+
+            return archive;
+        }
+
+        // PUT: api/Archives/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArchive(int id, Archive archive)
+        {
+            if (id != archive.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(archive).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArchiveExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Archives
+        [HttpPost]
+        public async Task<ActionResult<Archive>> PostArchive(Archive archive)
+        {
+            _context.Archives.Add(archive);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArchive", new { id = archive.Id }, archive);
+        }
+
+        // DELETE: api/Archives/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArchive(int id)
+        {
+            var archive = await _context.Archives.FindAsync(id);
+            if (archive == null)
+            {
+                return NotFound();
+            }
+
+            _context.Archives.Remove(archive);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArchiveExists(int id)
+        {
+            return _context.Archives.Any(e => e.Id == id);
+        }
+    }
+}
